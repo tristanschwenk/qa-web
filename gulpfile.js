ffdfcfc0bf3d23369f85fda0b0cfef47ebd03300
Tristan264 <tristan.bschwenk@icloud.com>
2024-07-04 11:19:43 +0200

    fix: change accessibility attributes over main and article pages

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,23 @@
-'use strict';
+"use strict";
 
-import * as dartSass from 'sass';
-import gulpSass from 'gulp-sass';
-import gulp from 'gulp';
+import * as dartSass from "sass";
+import gulpSass from "gulp-sass";
+import gulp from "gulp";
 
 const sass = gulpSass(dartSass);
 
-gulp.task('styles', () => {
-  return gulp.src('./src/**/*.scss')
-    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-    .pipe(gulp.dest('./src/'));
+gulp.task("styles", () => {
+  return gulp
+    .src("./assets/**/*.scss")
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(gulp.dest("./"));
 });
 
-gulp.task('build', gulp.parallel('styles'));
-gulp.task('watch', () => {
-  gulp.watch('./src/**/*.scss', gulp.parallel('styles'));
+gulp.task("build", gulp.parallel("styles"));
+gulp.task("watch", () => {
+  gulp.watch("./src/**/*.scss", gulp.parallel("styles"));
 });
 
-
-gulp.task('default', () => {
+gulp.task("default", () => {
   // do something
 });
